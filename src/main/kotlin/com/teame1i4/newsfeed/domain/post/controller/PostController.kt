c3ac914ec200166ba510f5bc28ad9a66523aa841
Alice <hifumialice@gmail.com>
2024-05-28 11:23:32 +0900

    :sparkles: feat: Implement DELETE api for post domain

--- src/main/kotlin/com/teame1i4/newsfeed/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/teame1i4/newsfeed/domain/post/controller/PostController.kt
@@ -1,4 +1,21 @@
 package com.teame1i4.newsfeed.domain.post.controller
 
-class PostController {
+import com.teame1i4.newsfeed.domain.post.service.PostService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/posts")
+@RestController
+class PostController(private val postService : PostService) {
+
+    @GetMapping("/{id}")
+    fun deletePost(@PathVariable(value = "id") id: Long) : ResponseEntity<Unit> {
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .body(postService.deletePost(id ))
+    }
 }
--- src/main/kotlin/com/teame1i4/newsfeed/domain/post/service/PostService.kt
+++ src/main/kotlin/com/teame1i4/newsfeed/domain/post/service/PostService.kt
@@ -1,4 +1,10 @@
 package com.teame1i4.newsfeed.domain.post.service
 
+import org.springframework.stereotype.Service
+
+@Service
 class PostService {
+    fun deletePost(id: Long) : Unit {
+        TODO("not implemented")
+    }
 }
