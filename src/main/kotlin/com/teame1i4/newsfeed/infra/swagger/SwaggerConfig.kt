4f98c89aa6085c550cff1b640462d012110969b2
AWKRID <minzoo202738@gmail.com>
2024-05-28 09:28:34 +0900

    :sparkles: feat: Add configuration for Swagger

---
+++ src/main/kotlin/com/teame1i4/newsfeed/infra/swagger/SwaggerConfig.kt
@@ -0,0 +1,21 @@
+package com.teame1i4.newsfeed.infra.swagger
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+        .components(Components())
+        .info(
+            Info()
+                .title("Music Newsfeed API")
+                .description("Newsfeed Schema")
+                .version("1.0.0")
+        )
+}
