785b2e892af9fb5104e633f409ff34408f7557c2
AWKRID <minzoo202738@gmail.com>
2024-05-28 11:18:13 +0900

    :sparkles: feat: Implement POST api for post domain

--- src/main/kotlin/com/teame1i4/newsfeed/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/teame1i4/newsfeed/domain/post/controller/PostController.kt
@@ -1,4 +1,28 @@
 package com.teame1i4.newsfeed.domain.post.controller
 
-class PostController {
+import com.teame1i4.newsfeed.domain.post.dto.CreatePostRequest
+import com.teame1i4.newsfeed.domain.post.dto.PostResponse
+import com.teame1i4.newsfeed.domain.post.service.PostService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/posts")
+@RestController
+class PostController(
+    private val postService: PostService
+) {
+
+    @PostMapping
+    fun createPost(@RequestBody createPostRequest: CreatePostRequest): ResponseEntity<PostResponse> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(postService.createPost(createPostRequest))
+    }
+
+
+
 }
