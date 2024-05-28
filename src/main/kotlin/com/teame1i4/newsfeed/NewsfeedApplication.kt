20c4506147ff5cc9c9c68dc6ffd40381287db460
AWKRID <minzoo202738@gmail.com>
2024-05-28 09:01:10 +0900

    :tada: init: New Project

---
+++ src/main/kotlin/com/teame1i4/newsfeed/NewsfeedApplication.kt
@@ -0,0 +1,11 @@
+package com.teame1i4.newsfeed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewsfeedApplication
+
+fun main(args: Array<String>) {
+    runApplication<NewsfeedApplication>(*args)
+}
---
+++ src/test/kotlin/com/teame1i4/newsfeed/NewsfeedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teame1i4.newsfeed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
