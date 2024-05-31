121d0cec5c86ca0b19a31b018de80897a74c7a5d
Alice <hifumialice@gmail.com>
2024-05-31 09:42:45 +0900

    :sparkles: feat: "Update get public posts"
    comment sort descend by createdAt
    Add member verification in comment CRUD

--- src/main/kotlin/com/teame1i4/newsfeed/domain/post/repository/PostRepository.kt
+++ src/main/kotlin/com/teame1i4/newsfeed/domain/post/repository/PostRepository.kt
@@ -1,17 +1,18 @@
 package com.teame1i4.newsfeed.domain.post.repository
 
 import com.teame1i4.newsfeed.domain.post.model.Post
+import com.teame1i4.newsfeed.domain.post.model.PostStatus
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface PostRepository : JpaRepository<Post, Long> {
-    @Query("select p from Post p where p.tags like concat('%#',:tag,'#%') order by p.createdAt desc")
-    fun findAllByTagOrderByCreatedAtDesc(@Param("tag") tag: String): List<Post>
+    @Query("select p from Post p where p.tags like concat('%#',:tag,'#%') and p.postStatus = 'PUBLIC' order by p.createdAt desc")
+    fun findAllByTagAndPostStatusOrderByCreatedAtDesc(@Param("tag") tag: String): List<Post>
 
-    fun findAllByTitleContainingOrderByCreatedAtDesc(title: String) : List<Post>
-    fun findAllByMusicTypeOrderByCreatedAtDesc(musicTypeId: String) : List<Post>
-    fun findAllByMemberIdOrderByCreatedAtDesc(memberId : Long) : List<Post>
-    fun findAllByOrderByCreatedAtDesc() : List<Post>
+    fun findAllByTitleContainingAndPostStatusOrderByCreatedAtDesc(title: String, postStatus: PostStatus): List<Post>
+    fun findAllByMusicTypeAndPostStatusOrderByCreatedAtDesc(musicTypeId: String, postStatus: PostStatus): List<Post>
+    fun findAllByMemberIdAndPostStatusOrderByCreatedAtDesc(memberId: Long, postStatus: PostStatus): List<Post>
+    fun findAllByPostStatusOrderByCreatedAtDesc(postStatus: PostStatus): List<Post>
 
 }
--- src/main/kotlin/com/teame1i4/newsfeed/domain/post/service/PostService.kt
+++ src/main/kotlin/com/teame1i4/newsfeed/domain/post/service/PostService.kt
@@ -1,18 +1,16 @@
 package com.teame1i4.newsfeed.domain.post.service
 
+import com.teame1i4.newsfeed.domain.comment.model.Comment
 import com.teame1i4.newsfeed.domain.exception.ModelNotFoundException
 import com.teame1i4.newsfeed.domain.exception.TypeNotFoundException
 import com.teame1i4.newsfeed.domain.post.dto.CreatePostRequest
 import com.teame1i4.newsfeed.domain.post.dto.PostResponse
 import com.teame1i4.newsfeed.domain.post.dto.PostWithCommentResponse
 import com.teame1i4.newsfeed.domain.post.dto.UpdatePostRequest
-import com.teame1i4.newsfeed.domain.post.model.Post
-import com.teame1i4.newsfeed.domain.post.model.toResponse
-import com.teame1i4.newsfeed.domain.post.model.toWithCommentResponse
 import com.teame1i4.newsfeed.domain.post.repository.MusicTypeRepository
 import com.teame1i4.newsfeed.domain.post.repository.PostRepository
 import com.teame1i4.newsfeed.domain.member.repository.MemberRepository
-import com.teame1i4.newsfeed.domain.post.model.MusicType
+import com.teame1i4.newsfeed.domain.post.model.*
 import jakarta.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
@@ -36,7 +34,8 @@ class PostService(
             tags = "#" + request.tags.joinToString("#") + "#"
         )
 
-        val musicType : MusicType = musicTypeRepository.findByIdOrNull(request.musicType) ?: throw TypeNotFoundException(request.musicType)
+        val musicType: MusicType =
+            musicTypeRepository.findByIdOrNull(request.musicType) ?: throw TypeNotFoundException(request.musicType)
         musicType.updateCountPost(true)
         musicTypeRepository.save(musicType)
 
@@ -48,7 +47,8 @@ class PostService(
         val post: Post = postRepository.findByIdOrNull(postId) ?: throw ModelNotFoundException("Post", postId)
         postRepository.delete(post)
 
-        val musicType : MusicType = musicTypeRepository.findByIdOrNull(post.musicType) ?: throw TypeNotFoundException(post.musicType)
+        val musicType: MusicType =
+            musicTypeRepository.findByIdOrNull(post.musicType) ?: throw TypeNotFoundException(post.musicType)
         musicType.updateCountPost(false)
         musicTypeRepository.save(musicType)
     }
@@ -56,7 +56,8 @@ class PostService(
     @Transactional
     fun updatePost(postId: Long, request: UpdatePostRequest): PostResponse {
         val post = postRepository.findByIdOrNull(postId) ?: throw ModelNotFoundException("Post", postId)
-        var musicType : MusicType = musicTypeRepository.findByIdOrNull(request.musicType) ?: throw TypeNotFoundException(request.musicType)
+        var musicType: MusicType =
+            musicTypeRepository.findByIdOrNull(request.musicType) ?: throw TypeNotFoundException(request.musicType)
 
         musicType.updateCountPost(false)
         musicTypeRepository.save(musicType)
@@ -72,11 +73,21 @@ class PostService(
 
     fun getPosts(tag: String?, title: String?, musicType: String?, memberId: Long?): List<PostResponse> {
 
-        val posts: List<Post> = if (!tag.isNullOrBlank()) postRepository.findAllByTagOrderByCreatedAtDesc(tag)
-        else if (!title.isNullOrBlank()) postRepository.findAllByTitleContainingOrderByCreatedAtDesc(title)
-        else if (!musicType.isNullOrBlank()) postRepository.findAllByMusicTypeOrderByCreatedAtDesc(musicType)
-        else if (memberId != null) postRepository.findAllByMemberIdOrderByCreatedAtDesc(memberId)
-        else postRepository.findAllByOrderByCreatedAtDesc()
+        val posts: List<Post> =
+            if (!tag.isNullOrBlank()) postRepository.findAllByTagAndPostStatusOrderByCreatedAtDesc(tag)
+            else if (!title.isNullOrBlank()) postRepository.findAllByTitleContainingAndPostStatusOrderByCreatedAtDesc(
+                title,
+                PostStatus.PUBLIC
+            )
+            else if (!musicType.isNullOrBlank()) postRepository.findAllByMusicTypeAndPostStatusOrderByCreatedAtDesc(
+                musicType,
+                PostStatus.PUBLIC
+            )
+            else if (memberId != null) postRepository.findAllByMemberIdAndPostStatusOrderByCreatedAtDesc(
+                memberId,
+                PostStatus.PUBLIC
+            )
+            else postRepository.findAllByPostStatusOrderByCreatedAtDesc(PostStatus.PUBLIC)
 
         return posts.map { it.toResponse() }
     }
@@ -85,6 +96,9 @@ class PostService(
     fun getPostById(postId: Long): PostWithCommentResponse {
         val post: Post =
             postRepository.findByIdOrNull(postId).also { it?.view() } ?: throw ModelNotFoundException("Post", postId)
+
+        post.comments.sortByDescending { it.createdAt }
+
         return post.toWithCommentResponse()
     }
 }
