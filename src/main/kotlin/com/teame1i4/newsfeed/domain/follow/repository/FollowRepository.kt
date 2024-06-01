75184b1d440c2aaba44339090df6ba6521a72b63
Alice <hifumialice@gmail.com>
2024-06-01 23:20:26 +0900

    :sparkles: feat: Implement Member's NewsFeed on follower's post

--- src/main/kotlin/com/teame1i4/newsfeed/domain/follow/repository/FollowRepository.kt
+++ src/main/kotlin/com/teame1i4/newsfeed/domain/follow/repository/FollowRepository.kt
@@ -4,6 +4,7 @@ import com.teame1i4.newsfeed.domain.follow.model.Follow
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface FollowRepository : JpaRepository<Follow, Long> {
-    fun existsByMemberId(memberId: Long): Boolean
+    fun existsByMemberIdAndFollowerMemberId(memberId: Long,followerMemberId : Long): Boolean
     fun findByMemberIdAndFollowerMemberId(memberId: Long, followerId: Long): Follow?
+    fun findAllByMemberId(memberId: Long): List<Follow>
 }
